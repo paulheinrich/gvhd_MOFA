92674e5f8d21737e533b0905c459590f0a470359
hep62703 <paul.heinrich@ukr.de>
2023-06-06 16:06:19 +0200

    Add MEFISTO analysis

---
+++ analysis/hep62703_2023-04-28_mefisto/scripts/fit_mofa_metabolites.R
@@ -0,0 +1,93 @@
+library(here)
+library(stringr)
+library(reticulate)
+library("MOFA2")
+
+here::i_am("analysis/hep62703_2023-04-28_mefisto/scripts/fit_mofa_metabolites.R")
+
+# Path to python binary with mofapy2 dependency
+# This is an absolute path and system dependent!
+
+path_mofapy2_python <- "/home/hep62703/.conda/envs/p107_hp_mofa_mofapy2/bin/python"
+
+# Load input data
+
+views <- c("16S", "ITS", "virome", "metabolites")
+input_data <- list()
+
+for (view in views) {
+    filename <- paste0("mofa_input_", view, ".csv")
+    input_data[[view]] <- as.matrix(read.csv(
+        here("data", filename),
+        header = TRUE,
+        row.names = 1,
+        check.names = FALSE
+    ))
+}
+
+# Load metadata
+
+metadata <- read.csv(
+    file = here("metadata/metadata_pat_model_combined.csv"),
+    header = TRUE
+)
+
+# Create MOFA object with metadata
+
+model <- create_mofa(input_data)
+
+# Create time coviariate in metadata
+
+time_mapping <- c(
+    "IDX" = -7,
+    "COND" = -7,
+    "0" = 0,
+    "7" = 7,
+    "14" = 14,
+    "21" = 21,
+    "28" = 28,
+    "POST" = 35
+)
+
+metadata$time <- sapply(metadata$amp_Timepoint, function(x) time_mapping[x])
+
+samples_metadata(model) <- metadata
+
+model <- set_covariates(model, covariates = "time")
+
+# Modify options
+
+model_opts <- get_default_model_options(model)
+model_opts$num_factors <- 10
+
+data_opts <- get_default_data_options(model)
+
+train_opts <- get_default_training_options(model)
+train_opts$convergence_mode <- "medium"
+
+mefisto_opts <- get_default_mefisto_options(model)
+
+# Prepare model
+
+model <- prepare_mofa(
+    object = model,
+    model_options = model_opts,
+    data_options = data_opts,
+    training_options = train_opts,
+    mefisto_options = mefisto_opts
+)
+
+# Train model
+
+reticulate::use_python(path_mofapy2_python)
+
+model_path <- here(
+    "analysis/hep62703_2023-04-28_mefisto/data/models",
+    "model_metabolites_medium_F10.hdf5"
+)
+
+model_trained <- run_mofa(
+    object = model,
+    outfile = NULL,
+    use_basilisk = FALSE
+)
